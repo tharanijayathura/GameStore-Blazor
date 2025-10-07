e91d73faac77c74a845438caf495f79b87dfbe5e
Tharani Jayathura <tharanijayathura1@gmail.com>
2025-10-07 22:59:07 +0530

    update GameStore.Frontend/Components/Pages/EditGame.razor update GameStore.Frontend/Models/GameDetails.cs

--- GameStore.Frontend/Models/GameDetails.cs
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -1,16 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using GameStore.Frontend.Components.Pages;
 
 namespace GameStore.Frontend.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
 
     public required string Name { get; set; }
 
+    [Required(ErrorMessage = "Genre is required")]
     public int GenreId { get; set; }
 
+    [Range(0, 100)]
     public decimal Price { get; set; }
+
 
     public DateOnly ReleaseDate { get; set; }
 }
